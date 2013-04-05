7d116c80a101fbfba1505c7d4e7b38ad6c51377c
Keith Gautreaux <keith.gautreaux@gmail.com>
2013-04-05 15:30:46 -0700

    Weird angular ng-view template generation using a lamba in the rails routes.  Temporary proof of concept.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Obs::Application.routes.draw do
   get 'obs' => 'obs#index'
+  get 'templates/index' => lambda { |hash| [200, {}, ["<p ng-model='name'>Fucking Works {{name}}!</p>"]]} 
+  get 'templates/edit' => 'templates#edit'
+  get 'templates/new' => 'templates#new'
 
   authenticated :user do
     root :to => 'home#index'
