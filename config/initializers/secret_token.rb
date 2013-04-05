d1f37d279520e7d32e4ce179ae46e1f053ce2dc7
Keith Gautreaux <keith.gautreaux@gmail.com>
2013-04-04 23:26:08 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Obs::Application.config.secret_token = '4a76b388cdf2f4384c421edb7e609970c426a773e3b36a0e28ad7a750ed67dda9f87297c032a64737430347171a9010c418e4a01e4a6419f4b01aca6b5705b72'
